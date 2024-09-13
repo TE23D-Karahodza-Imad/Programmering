5109e1b5914a423d9f41ed8c87f7c0b2fa71bfd3
Imad Karahodza <imadahmed.karahodza@elev.ga.ntig.se>
2024-09-13 11:20:32 +0200

    asdasdas

--- FrågesportÖvning/Program.cs
+++ FrågesportÖvning/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿Console.WriteLine("Welcome to my quiz about which football club belongs to a certain country");
+Console.WriteLine("Answer with a, b, c, or d. Goodluck..");
+//
+
+int points = 0;
+// Points är noll
+
+Console.WriteLine("1: Atletico Madrid is from which country?");
+Console.WriteLine("a) Germany b) Portugal c) Spain");
+string answer1 = Console.ReadLine().ToLower();
+//------------------------------------------------------//
+if(answer1 == "c")
+{
+    points++;
+    Console.WriteLine("Correct answer!");
+}
+else  
+{
+    Console.WriteLine("Wrong answer!.. noob!");
+}
+//------------------------------------------------------//
+Console.WriteLine("2: What country is Bayern from?");
+Console.WriteLine("a) Germany b) Russia c)Denmark or d) United Kingdom");
+string answer2 = Console.ReadLine().ToLower();
+
+if (answer2 == "a")
+{
+    points++;
+    Console.WriteLine("Correct answer!");
+}
+else  
+{
+    Console.WriteLine("Wrong answer!.. noob!");
+}
+//------------------------------------------------------//
+Console.WriteLine("Okay you are advancing slowly.");
+Console.WriteLine("What country does Liverpool come from?");
+Console.WriteLine("a) Hungary b) United States c)United Kingdom d) Australia");
+
+
+
+
+
+
+//------------------------------------------------------//
+Console.ReadLine();
